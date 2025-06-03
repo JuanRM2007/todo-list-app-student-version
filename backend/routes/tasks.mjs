5457b83d29f5c34b0ca40be51483340288b92174
JuanM <juanm389@nycstudents.net>
2025-06-03 13:27:42 -0400

    Update tasks.mjs

--- backend/routes/tasks.mjs
+++ backend/routes/tasks.mjs
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
-// There is a bug in line 4 you need to fix it
+// There is a bug in line 4 you need to fix it(fixed)
 const taskModel = require("../model/taskModel");
 
-//Write a comment describing the purpose of this route
+//Retrieves all tasks from the database then responds with an array.
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
   res.json(tasks);
 });
 
-// Write a comment describing the purpose of this route
+
+//Adds a new task to the database, requests a name and a description, responds with a new task and a 201 status.
+
 router.post("/", async (req, res) => {
-  //there is a bug in line 15 you need to fix
+  //there is a bug in line 15 you need to fix(fixed)
   const { name, description } = req.body;
   const task = await taskModel.addTask(name, description);
   res.status(201).json(task);
